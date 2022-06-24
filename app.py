5f47fbd40096017b5329f382703851c919b27559
Fredy Quispe Neira <fredy.quispe@ucsp.edu.pe>
2022-06-24 13:02:22 -0500

    add .gitignore and basic database connection

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 from flask import Flask, render_template, session
 from flask_session import Session
+from pymysql import *
+import pymysql
 
+#variables generales del servidor de BBDD
+servidor="db4free.net" 
+puerto=3306
+usuario="sanpablo22"
+contrasenha="sanpablo22"
+nombre_bd="parking_system"
 
+database= pymysql.connect(host=servidor, port=puerto, user=usuario, passwd=contrasenha, db=nombre_bd)
+
+cursor= database.cursor()
+cursor.execute("select * from Regiones;")
+consulta= cursor.fetchall()
+print(consulta)
+database.close()
+
+'''
 app= Flask(__name__)
 
 @app.route("/")
@@ -12,3 +29,4 @@ def init():
 
 if __name__=="__main__":
     app.run(debug=True)
+'''
